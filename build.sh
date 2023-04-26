3712c701646f47726036b64d6f57308b4490e1e5
Loïc Tosser <loic@kalvad.com>
2023-04-26 15:31:37 +0400

    node + umami version

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 [ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
-[ -z "$UMAMI_VERSION" ] && export UMAMI_VERSION="2.0.0"
+[ -z "$UMAMI_VERSION" ] && export UMAMI_VERSION="2.2.0"
 [ -z "$PORT" ] && export PORT="8080"
 
 rm -rf umami-$UMAMI_VERSION
